f1b46cb1bd1341b7a13dabcb68bf51dd01562581
Hitul Mistry <mistryhitul007@gmail.com>
2019-11-04 13:02:04 +0530

    Create python-server-setup.sh

---
+++ python-server-setup.sh
@@ -0,0 +1,22 @@
+sudo apt-get update
+sudo apt-get install -y supervisor nginx
+
+git clone https://github.com/pyenv/pyenv.git ~/.pyenv
+echo 'export PYENV_ROOT="$HOME/.pyenv"' >> ~/.bash_profile
+echo 'export PATH="$PYENV_ROOT/bin:$PATH"' >> ~/.bash_profile
+echo -e 'if command -v pyenv 1>/dev/null 2>&1; then\n  eval "$(pyenv init -)"\nfi' >> ~/.bash_profile
+exec "$SHELL"
+
+sudo apt-get -y install build-essential libncursesw5-dev libreadline-gplv2-dev libssl-dev libgdbm-dev libc6-dev libsqlite3-dev libbz2-dev libffi-dev
+sudo apt install -y zlib1g-dev libffi-dev libssl-dev libbz2-dev libncursesw5-dev libgdbm-dev liblzma-dev libsqlite3-dev tk-dev uuid-dev libreadline-dev 
+
+pyenv install 3.6.3
+
+git clone https://github.com/pyenv/pyenv-virtualenv.git $(pyenv root)/plugins/pyenv-virtualenv
+echo 'eval "$(pyenv virtualenv-init -)"' >> ~/.bash_profile
+exec "$SHELL"
+
+eval "$(pyenv init -)"
+eval "$(pyenv virtualenv-init -)"
+
+exec "$SHELL"
