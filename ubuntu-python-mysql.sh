2da9b721743b9cff739f801020bee5000edb53bc
Hitul Mistry <mistryhitul007@gmail.com>
2019-11-04 15:24:36 +0530

    Create ubuntu-python-mysql.sh

---
+++ ubuntu-python-mysql.sh
@@ -0,0 +1,6 @@
+sudo apt-get install libmysqlclient-dev
+sudo apt-get install python-mysql.connector
+sudo apt-get install python3-mysql.connector
+
+pip install mysqlclient
+pip install MySQL-python
