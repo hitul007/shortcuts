5b212b9c88eb049dd278166006343970813d788d
Hitul Mistry <mistryhitul007@gmail.com>
2020-04-04 17:46:24 +0530

    Update python-server-setup.sh

--- python-server-setup.sh
+++ python-server-setup.sh
@@ -22,3 +22,6 @@ echo 'eval "$(pyenv virtualenv-init -)"' >> ~/.bash_profile
 exec "$SHELL"
 
 exec "sudo apt-get install -y libpq-dev gcc python-dev"
+
+# Tensorflow
+exec "sudo apt-get -y install libtesseract-dev libleptonica-dev"
