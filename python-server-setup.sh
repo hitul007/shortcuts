4d5e6568142afe8d4eff00c6d07ea7643a55e013
Hitul Mistry <mistryhitul007@gmail.com>
2021-04-30 16:37:06 +0530

    Update python-server-setup.sh

--- python-server-setup.sh
+++ python-server-setup.sh
@@ -10,6 +10,9 @@ exec "$SHELL"
 sudo apt-get -y install build-essential libncursesw5-dev libreadline-gplv2-dev libssl-dev libgdbm-dev libc6-dev libsqlite3-dev libbz2-dev libffi-dev
 sudo apt install -y zlib1g-dev libffi-dev libssl-dev libbz2-dev libncursesw5-dev libgdbm-dev liblzma-dev libsqlite3-dev tk-dev uuid-dev libreadline-dev 
 
+# If YUM
+sudo yum install openssl-devel bzip2-devel libffi-devel gcc
+
 pyenv install 3.6.3
 
 git clone https://github.com/pyenv/pyenv-virtualenv.git $(pyenv root)/plugins/pyenv-virtualenv
