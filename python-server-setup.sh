08425bd94d88046bf8f50297e5592db0efd8dcea
Hitul Mistry <mistryhitul007@gmail.com>
2021-12-30 16:02:46 +0530

    Update python-server-setup.sh

--- python-server-setup.sh
+++ python-server-setup.sh
@@ -10,7 +10,7 @@ exec "$SHELL"
 sudo apt-get -y install build-essential libncursesw5-dev libreadline-gplv2-dev libssl-dev libgdbm-dev libc6-dev libsqlite3-dev libbz2-dev libffi-dev
 sudo apt install -y zlib1g-dev libffi-dev libssl-dev libbz2-dev libncursesw5-dev libgdbm-dev liblzma-dev libsqlite3-dev tk-dev uuid-dev libreadline-dev 
 
-# If YUM
+# If not using Ubuntu and using the redhat based system then only execute below command.
 sudo yum install openssl-devel bzip2-devel libffi-devel gcc
 
 pyenv install 3.6.3
